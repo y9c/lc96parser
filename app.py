3949cd00f2245e5cf02a3a9fcd1bf65cf9727615
Chang Ye <yech1990@gmail.com>
2023-11-09 02:31:49 -0600

    add plotly

--- app.py
+++ app.py
@@ -10,9 +10,10 @@ import os
 import tempfile
 
 import pandas as pd
+import plotly.express as px
 import streamlit as st
 
-from run import convert_file, run_lrp
+from run import run_lrp
 
 # Streamlit app layout
 st.title("lc96p qpcr Data Parser")
@@ -43,7 +44,29 @@ if uploaded_file is not None:
     with st.spinner("Converting the file... Please wait."):
         try:
             amp_table, result_table = run_lrp(temp_file_path)
-            st.line_chart(amp_table)
+            # st.line_chart(amp_table)
+
+            fig = px.line(amp_table, x=amp_table.index, y=amp_table.columns)
+            fig.update_layout(
+                clickmode="event+select",
+                legend=dict(
+                    orientation="h",
+                    yanchor="top",
+                    y=-0.12,
+                    xanchor="center",
+                    x=0.5,
+                    traceorder="normal",
+                    bgcolor=None,
+                    borderwidth=0,
+                    itemsizing="constant",
+                    tracegroupgap=0,
+                    entrywidth=1 / 12,
+                    entrywidthmode="fraction",
+                ),
+                height=550,
+            )
+            st.plotly_chart(fig, use_container_width=True, height=550)
+
             st.dataframe(
                 result_table.style.background_gradient(axis=None).format(
                     "{:.2f}"
