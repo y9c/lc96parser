02fbcf1dca165d56a0f1132a29feafcf56d5e35f
Chang Ye <yech1990@gmail.com>
2023-11-08 20:01:44 -0600

    quick update

---
+++ app.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2023 Ye Chang yech1990@gmail.com
+# Distributed under terms of the GNU license.
+
+
+import os
+import tempfile
+
+import streamlit as st
+
+from run import convert_file
+
+# Streamlit app layout
+st.title("lc96p qpcr Data Parser")
+
+
+# File uploader widget
+uploaded_file = st.file_uploader("Choose a PDF file", type="lc96p")
+if uploaded_file is not None:
+    with tempfile.NamedTemporaryFile(
+        delete=False, suffix=".lc96p"
+    ) as temp_file:
+        name_base = uploaded_file.name.rsplit(".")[0]
+        temp_file.write(uploaded_file.getvalue())
+        temp_file_path = temp_file.name
+    with st.spinner("Converting the file... Please wait."):
+        try:
+            file_base = os.path.splitext(temp_file_path)[0]
+            rdml_file = file_base + ".rdml"
+            excel_file = file_base + ".xlsx"
+            df_plate = convert_file(temp_file_path, rdml_file, excel_file)
+            # Display the table
+            st.write("Parsed Table:")
+            st.dataframe(
+                df_plate.style.background_gradient().format("{:.2f}"),
+                use_container_width=True,
+                height=(df_plate.shape[0] + 1) * 35 + 3,
+            )
+            # Provide download links for rdml_file and excel_file
+            col1, col2 = st.columns(2)
+            with col1:
+                st.download_button(
+                    label="Download RDML file",
+                    data=open(rdml_file, "rb"),
+                    file_name=name_base + ".rdml",
+                )
+            with col2:
+                st.download_button(
+                    label="Download Excel file",
+                    data=open(excel_file, "rb"),
+                    file_name=name_base + ".xlsx",
+                )
+            # Delete the temporary files
+            os.remove(temp_file_path)
+
+        except Exception as e:
+            st.error(f"An error occurred while parsing the lc96p file: {e}")
+
+hide_streamlit_style = """
+            <style>
+            [data-testid="stToolbar"] {visibility: hidden !important;}
+            footer {visibility: hidden !important;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # Copyright © 2022 Ye Chang yech1990@gmail.com
@@ -9,56 +9,63 @@
 import os
 import subprocess
 import sys
+import tempfile
 
 import pandas as pd
 
-if len(sys.argv) < 2:
-    sys.exit("Usage: run.py input_file.lc96p")
-
-input_file = sys.argv[1]
-
 # create 3 files for each input
-rdml_file = input_file.rsplit(".", 1)[0] + ".rdml"
-tsv_file = input_file.rsplit(".", 1)[0] + ".tsv"
-excel_file = input_file.rsplit(".", 1)[0] + ".xlsx"
 
 
-script_path = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), "rdmlpython", "rdml.py"
-)
+def convert_file(input_file, rdml_file, excel_file):
+    with tempfile.NamedTemporaryFile(
+        mode="w+", suffix=".tsv", delete=True
+    ) as tsv_temp:
+        tsv_file = tsv_temp.name
+        script_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "rdmlpython",
+            "rdml.py",
+        )
+        subprocess.call(
+            [
+                "python3",
+                script_path,
+                "-lrp",
+                input_file,
+                "--pcrEfficiencyExl",
+                "0.05",
+                "--excludeNoPlateau",
+                "--excludeEfficiency",
+                "mean",
+                "--timeRun",
+                "-o",
+                rdml_file,
+                "--saveResults",
+                tsv_file,
+            ]
+        )
+        df = pd.read_csv(tsv_file, sep="\t").loc[
+            :, ["well", "Cq (mean eff) - no plateau - stat efficiency"]
+        ]
 
-
-subprocess.call(
-    [
-        "python3",
-        script_path,
-        "-lrp",
-        input_file,
-        "--pcrEfficiencyExl",
-        "0.05",
-        "--excludeNoPlateau",
-        "--excludeEfficiency",
-        "mean",
-        "--timeRun",
-        "-o",
-        rdml_file,
-        "--saveResults",
-        tsv_file,
-    ]
-)
-
-
-df = pd.read_csv(tsv_file, sep="\t").loc[
-    :, ["well", "Cq (mean eff) - no plateau - stat efficiency"]
-]
-df_plate = (
-    df.join(df["well"].str.extract(r"(?P<letter>[A-Z])(?P<digit>\d+)"))
-    .assign(digit=lambda x: x.digit.astype(int))
-    .pivot(
-        index="letter",
-        columns="digit",
-        values="Cq (mean eff) - no plateau - stat efficiency",
+    df_plate = (
+        df.join(df["well"].str.extract(r"(?P<letter>[A-Z])(?P<digit>\d+)"))
+        .assign(digit=lambda x: x.digit.astype(int))
+        .pivot(
+            index="letter",
+            columns="digit",
+            values="Cq (mean eff) - no plateau - stat efficiency",
+        )
     )
-)
 
-df_plate.to_excel(excel_file, sheet_name="quant")
+    df_plate.to_excel(excel_file, sheet_name="quant")
+    return df_plate
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.exit("Usage: run.py input_file.lc96p")
+    input_file = sys.argv[1]
+    rdml_file = input_file.rsplit(".", 1)[0] + ".rdml"
+    excel_file = input_file.rsplit(".", 1)[0] + ".xlsx"
+    convert_file(input_file, rdml_file, excel_file)
